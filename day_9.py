c0577c9ce7c9ea8182f2564d43e156a1367c362e
Aravpal16 <36320357+Aravpal16@users.noreply.github.com>
2019-05-25 16:21:34 +0530

    Update day_9.py

--- day_9.py
+++ day_9.py
@@ -19,3 +19,50 @@ def get_circular_prime_count(limit):
     return counter
 #Provide different values for limit and test your program
 print(get_circular_prime_count(1000))
+
+#PF-Assgn-61
+import re
+def validate_name(name):
+    if(len(name)!=0 and len(name)<=15 and name.isalpha()):
+            return True
+    else:
+            return False
+
+def validate_phone_no(phno):
+    count = 0
+    if(len(phno)==10 and phno.isdigit()):
+        for i in range(1,len(phno)): 
+            if (phno[i] == phno[i-1]): 
+                count = count + 1 
+        if(count!=len(phno)-1):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def validate_email_id(email_id):
+    if(email_id.endswith(".com") and email_id.count("com")==1 and email_id.count("@")==1):
+        if("gmail" in email_id or "yahoo" in email_id or "hotmail" in email_id):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def validate_all(name,phone_no,email_id):
+    count = 0
+    if(validate_name(name)):
+        count = count + 1
+    else:    
+        print("Invalid Name")    
+    if(validate_phone_no(phone_no)):
+        count = count + 1
+    else:    
+        print("Invalid phone number")
+    if(validate_email_id(email_id)):
+        count  = count + 1
+    else:    
+        print("Invalid email id")
+    if(count==3):
+        print("All the details are valid")
